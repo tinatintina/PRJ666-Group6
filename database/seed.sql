e20cb5aad216a041a783c2d3633661477afbd934
zenamohamed <zena2149@gmail.com>
2026-09-17 21:45:59 -0400

    Create seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,12 @@
+USE smart_spend;
+
+INSERT INTO categories (category_name, category_type)
+VALUES
+('Food', 'Expense'),
+('Housing', 'Expense'),
+('Transportation', 'Expense'),
+('Entertainment', 'Expense'),
+('Education', 'Expense'),
+('Employment', 'Income'),
+('Scholarship', 'Income'),
+('Other Income', 'Income');
